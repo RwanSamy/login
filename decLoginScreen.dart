ac92dd775d8076ffef2b4c83703d9c93d3250016
Rwan Samy <160716744+RwanSamy@users.noreply.github.com>
2024-02-23 00:11:45 +0200

    Add files via upload

---
+++ decLoginScreen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class decLoginScreen extends StatefulWidget {
+  const decLoginScreen({super.key});
+
+  @override
+  State<decLoginScreen> createState() => _decLoginScreenState();
+}
+class _decLoginScreenState extends State<decLoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
